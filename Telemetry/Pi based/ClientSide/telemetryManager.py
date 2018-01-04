b26aa040b8ee8c4fe53152cdbeacc2ef9208b8df
Corey Hulse <cohulse@asu.edu>
2018-01-03 19:57:47 -0600

    refactored names

--- Telemetry/Pi based/ClientSide/telemetryManager.py
+++ Telemetry/Pi based/ClientSide/telemetryManager.py
@@ -1,7 +1,7 @@
-from telemetryPIClient import Client
-from telemetryFileHandler import TelemetryFileHandler
+from clientToWebsiteSerber import Client
+from serialMonitor import SerialMonitor
 
-class TelemetrySystem():
+class TelemetryManager():
 
     def __init__(self, server= "127.0.0.1", serverPort= 80, filePort= 5003, COM= "/dev/ttyACM0"):
         self.webSiteClient = Client(IP= server, port= serverPort, COM= COM)
@@ -24,5 +24,5 @@ if __name__ == "__main__":
     websiteServer = ""
     serverPort = 5002
     filePort = 5003
-    system = TelemetrySystem(server= websiteServer, serverPort= serverPort, filePort= filePort)
+    system = TelemetryManager(server= websiteServer, serverPort= serverPort, filePort= filePort)
     system.run()
