4b573220a72bdede2656aa0faff8181d6573cd0b
Corey Hulse <cohulse@asu.edu>
2018-01-18 20:44:31 -0600

    finished except for specific thresholds

--- Telemetry/Pi based/ClientSide/telemetryManager.py
+++ Telemetry/Pi based/ClientSide/telemetryManager.py
@@ -1,28 +1,42 @@
-from clientToWebsiteSerber import Client
+from emailNotifier import EmailNotifier
+from thingSpeakClient import ThingSpeakClient
 from serialMonitor import SerialMonitor
+
+testChannelID = "405105"
+testAPIkey = "X7LDFNI8L2IBFITP"
 
 class TelemetryManager():
 
-    def __init__(self, server= "127.0.0.1", serverPort= 80, filePort= 5003, COM= "/dev/ttyACM0"):
-        self.webSiteClient = Client(IP= server, port= serverPort, COM= COM)
-        self.webSiteClient.start()
+    dataNames = {"Moisture" : 'field1', 'Temperature' : 'field2'}
+    dataThresholds = {}
 
-        self.initializeServer()
-
-    def initializeServer(self):
-        self.server = socket.socket()
-        self.server.bind(("0.0.0.0", self.filePort))
-        self.server.listen(5)
+    def __init__(self, COM= "COM19"):
+        self.serialConnection = SerialMonitor(COM)
+        self.emailHandler = EmailNotifier('coreyohulse@gmail.com', 'kenshin247')
+        self.thingSpeakHandler = ThingSpeakClient(testChannelID, testAPIkey)
+        
 
     def run(self):
         while True:
-            client, clientIP = self.server.accept()
-            fileHandler = TelemetryFileHandler(client)
-            fileHandler.start()
+            message = self.serialConnection.getLineFromComPort()
+            print(message)
+            dataName, dataReading = message.split(',')
+            self.updateWebsite(dataName, dataReading)
+            self.checkForRedFlags(dataName, dataReading)
+
+    def updateWebsite(self, dataName, dataReading):
+        dataFields = [TelemetryManager.dataNames[dataName]]
+        dataReadings = [float(dataReading)]
+        self.thingSpeakHandler.makePost(dataFields, dataReadings)
+
+    def checkForRedFlags(self, dataName, dataReading):
+        if dataName == 'Temperature' and int(dataReading) <= 50:
+            message = "Hey {}, \nI am a notification bot, I am in testing mode. A red flag has been raised when reading " + dataName + ". The temperature was reading: {}. Let Corey know if you got this message"
+            self.emailHandler.notifyUsers(message, dataReading)
+            print('done')            
+        
 
 if __name__ == "__main__":
-    websiteServer = ""
-    serverPort = 5002
-    filePort = 5003
-    system = TelemetryManager(server= websiteServer, serverPort= serverPort, filePort= filePort)
+
+    system = TelemetryManager()
     system.run()
